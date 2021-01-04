df8fc47f4062b396828c6117932fdc617b8a1456
YenuPark <yenuzzang@naver.com>
2021-01-04 22:53:35 +0900

    C++ MemMalFree

--- CH2/문제02-2.cpp
+++ CH2/문제02-2.cpp
@@ -1,14 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS //strcpy 사용할때 정의해주기
+
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 
+char* MakeStrAdr(int len) {
+	char* str = (char*)malloc(sizeof(char) * len);
+	return str;
+}
+
 int main(void) {
-	const int num = 12;
-	const int* ptr = &num;
-	const int* (&ref) = ptr; 
-
-	cout << *ptr << endl;
-	cout << *ref << endl;
-
+	char* str = MakeStrAdr(20);
+	strcpy(str, "I am so happy~"); //문자열 복사 함수 i am so happy~ 를 str에 복사
+	cout << str << endl;
+	free(str);
 	return 0;
 
 }
