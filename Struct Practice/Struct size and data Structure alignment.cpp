1178ecab054db78d2568e697d5b3cc53d19fac11
YenuPark <yenuzzang@naver.com>
2021-01-05 22:18:40 +0900

    C++ Struct size and data Struct Alignment

---
+++ Struct Practice/Struct size and data Structure alignment.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+struct Employee
+{
+	short id;
+	int age;
+	double wage;
+};
+
+int main()
+{
+	std::cout << "The size of Employee is " << sizeof(Employee);
+
+	return 0;
+}
+
+// The size of Employee is 16
+//The reason is For performance reasons, the compiler sometimes adds gaps to the struct.
+//THIS IS CALLED PADDING
+//In the Employee struc above, the compiler adds 2bytes of invisible padding
+//after the member id to make the struct size 16bytes instead of 14 bytes
+
+​
