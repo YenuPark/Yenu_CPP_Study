1f2cbf9a2e84b7168afaf953c4b56c6393212ad7
YenuPark <yenuzzang@naver.com>
2021-01-05 22:06:37 +0900

    C++ Struct Declare & Define

---
+++ Struct Practice/DeclaringANDDefining.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Myinfo
+{
+	short id;
+	int age;
+	char name; //멤버(member) or 필드(field)
+};
+
+int main(void) {
+
+	Myinfo info;
+	//info라는 구조체 Myinfo 타입의 변수 정의 >> 해당 변수에 메모리 할당
+	
+	
+	return 0;
+}
